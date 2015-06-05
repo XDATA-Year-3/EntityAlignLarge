64c2be2437243201905a5ed48ae88ce3f3aa8720
Curtis Lisle <clisle@knowledgevis.com>
2015-06-05 11:28:59 -0400

    re-brand as Resonant;  added neighborhood columns to LineUP

--- service/lineupdatasetdescription.py
+++ service/lineupdatasetdescription.py
@@ -16,8 +16,8 @@ def run():
     response['primaryKey'] = 'entity'
     response['separator'] = '\t'
     response['url'] = 'service/lineupdataset'
-    response['columns'] = [ {'column': 'entity', 'type': 'string'},{'column': 'lev','type':'number', 'domain':[0,1]},{'column': 'substring','type':'number', 'domain':[0,1]}]
-    response['layout'] = {'primary': [   {'column': 'entity', 'width':100},{'column': 'lev','width':100}, {'column': 'substring','width':100},  {"type": "stacked","label": "Combined", "children": [{'column': 'lev','width':100}, {'column': 'substring','width':100}]}]}
+    response['columns'] = [ {'column': 'entity', 'type': 'string'},{'column': 'lev','type':'number', 'domain':[0,1]},{'column': 'substring','type':'number', 'domain':[0,1]}, {'column': '1hop','type':'number', 'domain':[0,100]}, {'column': '2hop','type':'number', 'domain':[0,1000]}, {'column': '3hop','type':'number', 'domain':[0,5000]}]
+    response['layout'] = {'primary': [   {'column': 'entity', 'width':100},{'column': 'lev','width':75}, {'column': 'substring','width':75},  {'column': '1hop','width':50}, {'column': '2hop','width':50}, {'column': '3hop','width':50}, {"type": "stacked","label": "Combined", "children": [{'column': 'lev','width':50}, {'column': 'substring','width':50}, {'column': '1hop','width':50}, {'column': '2hop','width':50}, {'column': '3hop','width':50}]}]}
 
 
     #tangelo.log(str(response))
